28795a83949820d9264cd207d9a94f11c9f3a53b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-05-15 18:15:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -x
+
+HEP_IP=${HEP_IP:-"127.0.0.1"}
+HEP_PORT=${HEP_PORT:-"127.0.0.1"}
+#sed -i "s/advertised_address=.*/advertised_address=\"${HEP_IP}\"/g" /etc/asterisk/hep.conf
+#sed -i "s/advertised_address=.*/advertised_address=\"${HEP_PORT}\"/g" /etc/asterisk/hep.conf
+
 echo "Starting Asterisk..."
-/usr/sbin/asterisk -vvvvvvv
+tail -f /var/log/asterisk/messages &&
+/usr/sbin/asterisk -fvvvvvvv
